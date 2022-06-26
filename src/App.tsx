256d6e90e35f476d4aff94b5b1a8b4d664a12d34
Felipe Costa <felipech3344@gmail.com>
2022-06-26 19:24:41 -0300

    Criação da estrutura da pagina de eventos, heade, sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,11 @@
-import { gql, useQuery } from "@apollo/client"
-import { useEffect } from "react"
-import { client } from "./lib/apollo"
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-
-interface Lesson {
-  id: string;
-  title: string;
-}
+import { Events } from "./pages/Events"
 
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
-
-  console.log(data);
-
-  return (
-    <ul>
-      {data?.lessons.map(lesson => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
-  )
+  return(
+    <div>
+      <Events />
+    </div>
+  ) 
 }
 
 export default App
