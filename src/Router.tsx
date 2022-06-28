d9cc0d507120ed90ccce8156bdc07171ea570870
Felipe Costa <felipech3344@gmail.com>
2022-06-27 21:34:18 -0300

    Criação da pagina de de inscrição, integração formulario com GraphCMS, atualização de rotas e finalização do conteúdo do player de video.

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { Events } from "./pages/Events";
+import { Subscribe } from "./pages/Subscribe";
 
 
 export function Router() {
     return (
         <Routes>
-            <Route path="/" element={<h1>Home</h1>}/>
+            <Route path="/" element={<Subscribe/>}/>
             <Route path="/event" element={<Events />} />
             <Route path="/event/lesson/:slug" element={<Events />} />
         </Routes>
