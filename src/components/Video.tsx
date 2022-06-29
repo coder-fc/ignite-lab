1d3969a28a63d1ce960fdc9d3cfa3a13afa4adff
Felipe Costa <felipech3344@gmail.com>
2022-06-29 18:33:37 -0300

    Ajustando o layout responsivo da parte do video.

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -63,8 +63,8 @@ export function Video(props: VideoProps) {
             </div>
 
             <div className="p-8 max-w-[1100px] mx-auto">
-                <div className="flex items-start gap-16">
-                    <div className="flex-1">
+                <div className="flex items-start gap-16 flex-col lg:flex-row">
+                    <div className="lg:flex-1">
                         <h1 className="text-2xl font-bold">
                             {data.lesson.title}
                         </h1>  
@@ -86,7 +86,7 @@ export function Video(props: VideoProps) {
                         </div>
                     </div>
                     
-                    <div className="flex flex-col gap-4">
+                    <div className="flex flex-col gap-4 w-full lg:w-[240px]">
                         <a href="#" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
                             <DiscordLogo  size={24}/>
                             Comunidade do discord
@@ -98,7 +98,7 @@ export function Video(props: VideoProps) {
                     </div>
                 </div>
 
-                <div className="gap-8 mt-20 grid grid-cols-2">
+                <div className="gap-8 mt-20 grid min-w-[360px] lg:grid-cols-2">
                     <a href="" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
                         <div className="bg-green-700 h-full p-6 flex items-center">
                             <FileArrowDown size={40}/>
